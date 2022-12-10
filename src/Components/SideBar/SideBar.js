d09f1b343df93091fd592131898e1ad137ac2b8a
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-12-11 03:08:02 +0530

    Added fake data

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,15 +1,32 @@
 import "./SideBar.scss"
-import React from "react";
+import {useState, useEffect} from "react";
 import Header from "./Header";
 import Search from '../Search';
 import MessagesTray from './MessagesTray';
 import MessageBox from '../MessageBox';
 
 const SideBar = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages();
+  }, []);
+
   return <div className="sideBar">
     <Header />
     <Search />
     <MessagesTray >
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
+      <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
       <MessageBox name="Vivek" message="How are you doing ?" time="Yesterday"/>
     </MessagesTray >
   </div>;
