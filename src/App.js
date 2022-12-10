edcce47025ba863217611c87e60dcdbbcd7d3534
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-12-10 14:16:07 +0530

    Added React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import store from './Store';
+import DashBoard from './Pages/DashBoard/DashBoard';
 import { Provider } from "react-redux";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<DashBoard/>}/>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
---
+++ src/Pages/DashBoard/DashBoard.js
@@ -0,0 +1,8 @@
+import "./DashBoard.scss";
+import React from "react";
+
+const DashBoard = () => {
+  return <div>DashBoard</div>;
+};
+
+export default DashBoard;
